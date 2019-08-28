702313bdf3ec1aa11249ac7ce8f36da0fb923e3a
Pritimay Sarkar <pritimay.sarkar@rupeek.com>
2019-08-28 14:39:41 +0530

    access routes for sitemap

--- app.js
+++ app.js
@@ -75,6 +75,19 @@ const sitemap = createSitemap({
     ]
 });
 
+let route;
+
+app._router.stack.forEach(function(r){
+    if (r.route && r.route.path){
+      console.log(r.route.path)
+    } else if (r.name === 'router') {
+        r.handle.stack.forEach(function(handler) {
+            route = handler.route;
+            route && console.log(route);
+        })
+    }
+  })
+
 app.get('/sitemap.xml', function (req, res) {
     try {
         const xml = sitemap.toXML()
